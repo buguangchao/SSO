795f806e27cf3ac2fbe7b683de91581766ad5d0d
bugc <1027700192@qq.com>
2018-03-29 17:22:29 +0800

    添加项目文件。

---
+++ AuthServer/Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace AuthServer.Configuration
+{
+    public class InMemoryConfiguration
+    {
+        /// <summary>
+        /// 1. 哪些API可以使用这个authorization server.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource> {
+                new ApiResource("socialnetwork", "社交网络")
+            };
+        }
+
+        /// <summary>
+        /// 2. 那些客户端Client(应用)可以使用这个authorization server.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client> {
+                new Client{
+                    ClientId ="socialnetwork",
+                    ClientSecrets = new []{ new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new []{ "socialnetwork"}
+                }
+            };
+        }
+
+        /// <summary>
+        /// 3. 指定可以使用authorization server授权的用户.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<TestUser> GetTestUsers()
+        {
+            return new[] {
+                new TestUser {
+                    SubjectId = "1",
+                    Username = "bugc",
+                    Password = "bugc"
+                }
+            };
+        }
+    }
+}
---
+++ AuthServer/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+using AuthServer.Configuration;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AuthServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                //.AddDeveloperSigningCredential()
+                .AddSigningCredential(new X509Certificate2(@"E:\bugc\CoreDemo\AuthServer\AuthServer\Pfx\socialnetwork.pfx", "123456"))
+                .AddTestUsers(InMemoryConfiguration.GetTestUsers().ToList())
+                .AddInMemoryApiResources(InMemoryConfiguration.GetApiResources())
+                .AddInMemoryClients(InMemoryConfiguration.GetClients());
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseIdentityServer();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
