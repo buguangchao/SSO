5b4a21a1d7957374170b3caa1a58a9a636d015b5
bugc <1027700192@qq.com>
2018-03-29 18:01:05 +0800

    添加token验证

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -30,10 +30,14 @@ namespace WebApi
             {
                 c.SwaggerDoc("V1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "My API", Version = "v1" });
             });
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
-            //});
+
+            //webapi配置identity server就需要对token进行验证, 这个库就是对access token进行验证的.
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(o=> {
+                    o.RequireHttpsMetadata = false;
+                    o.Authority = "http://localhost:5000";
+                    o.ApiName = "socialnetwork";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,15 +55,10 @@ namespace WebApi
                 o.SwaggerEndpoint("/swagger/V1/swagger.json", "MY API V1");
             });
 
-            //// Enable middleware to serve generated Swagger as a JSON endpoint.
-            //app.UseSwagger();
-
-            //// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-            //app.UseSwaggerUI(c =>
-            //{
-            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            //});
-
+            //这句话就是在把验证中间件添加到管道里, 这样每次请求就会调用验证服务了. 一定要在UserMvc()之前调用.
+            //当在controller或者Action使用[Authorize]属性的时候, 这个中间件就会基于传递给api的Token来验证Authorization, 
+            //如果没有token或者token不正确, 这个中间件就会告诉我们这个请求是UnAuthorized(未授权的).
+            app.UseAuthentication();
 
             app.UseMvc();
         }
